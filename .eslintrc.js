712a410d4cd2dd069f7054cd7dc468d4e60070b7
ivgtr <ivgtr.me@gmail.com>
2021-08-04 12:15:01 +0900

    :tada: init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    "eslint-config-sumikko",
+    "eslint-config-sumikko/ts",
+    "eslint-config-sumikko/react",
+    "eslint-config-sumikko/prettier",
+  ],
+  rules: {
+    "node/no-unpublished-import": [
+      "error",
+      {
+        allowModules: ["vite", "@vitejs/plugin-react-refresh"],
+      },
+    ],
+    "arrow-body-style": 0,
+    "react/jsx-uses-react": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/display-name": 0,
+  },
+};
---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-4xl">Hello world</p>
+    </div>
+  );
+};
+
+export default App;
